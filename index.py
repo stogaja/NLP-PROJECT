f344018f2b759f7ea6895ed141cabc4e46884b4e
Steve <60295400+stogaja@users.noreply.github.com>
2022-09-15 20:33:02 +0300

    Add files via upload

--- index.py
+++ index.py
@@ -1,3 +1,4 @@
+import io
 import netrc
 import pickle
 import sys
@@ -5,8 +6,6 @@ import pandas as pd
 import streamlit as st
 # let's import sentence transformer
 import sentence_transformers
-
-import plotly.express as px
 import torch
 #######################################
 
@@ -28,12 +27,22 @@ st.markdown(
 """,
     unsafe_allow_html=True,
 )
-# let's load the saved model
-loaded_model = pickle.load(open('XpathFinder1.sav', 'rb'))
 
-# let's enable CUDA if available
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print("Using device:", device)
+# # let's load the saved model
+# loaded_model = pickle.load(open('XpathFinder1.sav', 'rb'))
+#loaded_model = pickle.load('XpathFinder1.sav', map_location='cpu')
+
+
+class CPU_Unpickler(pickle.Unpickler):
+    def find_class(self, module, name):
+        if module == 'torch.storage' and name == '_load_from_bytes':
+            return lambda b: torch.load(io.BytesIO(b), map_location='cpu')
+        else:
+            return super().find_class(module, name)
+
+
+loaded_model = CPU_Unpickler(open('XpathFinder1.sav', 'rb')).load()
+
 
 # here is how to create containers
 header_container = st.container()
@@ -48,12 +57,12 @@ with header_container:
 
 # Another container
 with stats_container:
-   # importing data from csv files
-    data = pd.read_csv('streamlit template/JC-202103-citibike-tripdata.csv')
+   # # importing data from csv files
+   #  data = pd.read_csv('streamlit template/JC-202103-citibike-tripdata.csv')
 
-    # 5 --- You can work with data, change it and filter it as you always do using Pandas or any other library
-    start_station_list = ['All'] + data['start station name'].unique().tolist()
-    end_station_list = ['All'] + data['end station name'].unique().tolist()
+   #  # 5 --- You can work with data, change it and filter it as you always do using Pandas or any other library
+   #  start_station_list = ['All'] + data['start station name'].unique().tolist()
+   #  end_station_list = ['All'] + data['end station name'].unique().tolist()
 
     # 6 --- collecting input from the user
     #		Steamlit has built in components to collect input from users
@@ -62,6 +71,12 @@ with stats_container:
     text_input = st.text_input("Enter the text in the space below ...")
 
    # let's pass the input to the model
-prediction = loaded_model.predict([text_input])[0]
-      # let's display the prediction
-st.write(f"Prediction: {prediction}")
+   # let's pass the input to the model
+    if text_input:
+        # let's get the input from the user
+        input_text = text_input
+        # let's get the prediction
+        prediction = loaded_model.predict([input_text])
+        # let's print the prediction
+        st.write(f"Your input: {input_text}")
+        st.write(f"Your prediction: {prediction[0]}")
