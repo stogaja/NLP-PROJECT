65a986c774be41060640a025c2334b1280b1ac7f
Steve <60295400+stogaja@users.noreply.github.com>
2022-09-16 16:07:35 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import netrc
 import pickle
 import sys
 import pandas as pd
+import numpy as np
 import streamlit as st
 # let's import sentence transformer
 import sentence_transformers
@@ -33,36 +34,49 @@ loaded_model = pickle.load(open('XpathFinder1.sav', 'rb'))
 #loaded_model = pickle.load('XpathFinder1.sav', map_location='cpu')
 
 
-#class CPU_Unpickler(pickle.Unpickler):
-#    def find_class(self, module, name):
-#        if module == 'torch.storage' and name == '_load_from_bytes':
-#            return lambda b: torch.load(io.BytesIO(b), map_location='cpu')
-#        else:
-#            return super().find_class(module, name)
-#
 
 #loaded_model = CPU_Unpickler(open('XpathFinder1.sav', 'rb')).load()
 
 
-# here is how to create containers
+# Containers
 header_container = st.container()
-stats_container = st.container()
+mod_container = st.container()
 
-# the header
+# Header
 with header_container:
 
     # different levels of text you can include in your app
-    st.title("Semantic Text Similarity App")
-    st.header("Hello!, Let's match some words!!!")
+    st.title("Xpath Finder App")
 
-# Another container
-with stats_container:
-    # collect input using free text
-    text_input = st.text_input("Enter the text in the space below ...")
+
+# model container
+with mod_container:
+    # collecting input from user
+    prompt = st.text_input("Enter your description below ...")
+
+    # Loading e data
+    data = (pd.read_csv("/content/SBERT_data.csv")).drop(['Unnamed: 0'], axis = 1)
+
+    data['prompt']= prompt
+    data.rename(columns = {'target_text':'sentence2', 'prompt':'sentence1'}, inplace = True)
+    data['sentence2'] = data['sentence2'].astype('str')
+    data['sentence1']  = data['sentence1'].astype('str')
 
     # let's pass the input to the loaded_model with torch compiled with cuda
-    if text_input:
+    if prompt:
         # let's get the result
-        result = loaded_model.predict([text_input])
-        # let's show the result
-        st.write(result)
+        simscore = XpathFinder.predict([prompt])
+        from sentence_transformers import CrossEncoder
+        XpathFinder = CrossEncoder("cross-encoder/stsb-roberta-base")
+        sentence_pairs = []
+        for sentence1, sentence2 in zip(data['sentence1'],data['sentence2']):
+          sentence_pairs.append([sentence1, sentence2])
+        
+        # sorting the df to get highest scoring xpath_container
+        data['SBERT CrossEncoder_Score'] = XpathFinder.predict(sentence_pairs)
+        most_acc = data.head(5)
+        # predictions
+        st.write("Highest Similarity score: ", simscore)
+        st.text("Is this one of these the Xpath you're looking for?")
+        st.write(st.write(most_acc["input_text"])) 
+        
