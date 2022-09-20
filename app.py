19eddebf05b8411dd65b994463ee5d709f297685
Steve <60295400+stogaja@users.noreply.github.com>
2022-09-21 00:09:41 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import numpy as np
 import streamlit as st
 # from sentence_transformers import SentenceTransformer
 # import sentence_transformers
-# import torch
+import torch
 #######################################
 
 st.markdown(
